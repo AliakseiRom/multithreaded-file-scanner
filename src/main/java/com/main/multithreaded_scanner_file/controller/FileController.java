8724f2289381ebfc2cfd5361e644d34c563e1189
AliakseiRom <aliaksei.ramanouskij@gmail.com>
2025-06-25 13:20:29 +0300

    FileController refactor

--- src/main/java/com/main/multithreaded_scanner_file/controller/FileController.java
+++ src/main/java/com/main/multithreaded_scanner_file/controller/FileController.java
@@ -1,8 +1,29 @@
 package com.main.multithreaded_scanner_file.controller;
 
+import com.main.multithreaded_scanner_file.dto.FileRequestDTO;
+import com.main.multithreaded_scanner_file.dto.FileResponseDTO;
+import com.main.multithreaded_scanner_file.service.ScanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/scan")
 public class FileController {
 
+    @Autowired
+    private ScanService scanService;
+
+    @GetMapping
+    public ResponseEntity<FileResponseDTO> fileScan (@RequestBody FileRequestDTO fileRequestDTO) {
+        return new ResponseEntity<>(scanService.scan(
+                fileRequestDTO.path(),
+                fileRequestDTO.mask()),
+                HttpStatus.OK
+        );
+    }
 }
