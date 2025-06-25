6eac46843b4ae10fbac5c9388d39226f956e08c9
AliakseiRom <aliaksei.ramanouskij@gmail.com>
2025-06-25 13:19:13 +0300

    rename FileService and mapper realization

--- src/main/java/com/main/multithreaded_scanner_file/service/ScanService.java
+++ src/main/java/com/main/multithreaded_scanner_file/service/ScanService.java
@@ -1,5 +1,7 @@
 package com.main.multithreaded_scanner_file.service;
 
+import com.main.multithreaded_scanner_file.dto.FileResponseDTO;
+import com.main.multithreaded_scanner_file.mapper.FileMapper;
 import com.main.multithreaded_scanner_file.model.FileEntity;
 import com.main.multithreaded_scanner_file.repo.FileEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,39 +11,49 @@ import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Service
-public class FileService {
+public class ScanService {
 
     @Autowired
     private FileEntityRepository fileRepository;
+
+    @Autowired
+    private FileMapper fileMapper;
+
     private final ExecutorService executor = Executors.newFixedThreadPool(10);
 
-    public void scan(String path, String mask) {
-        Path dir = Paths.get(path);
-        CompletableFuture<Void> future = scanRecursive(dir, mask);
-        future.thenRun(() -> System.out.println("Scanning is done"));
+    public FileResponseDTO scan(String path, String mask) {
+        List<FileEntity> currentScanResults = Collections.synchronizedList(new ArrayList<>());
+
+        CompletableFuture<Void> future = scanRecursive(Path.of(path), mask, currentScanResults);
+        future.join();
+
+        return fileMapper.toResponseDTO(path, currentScanResults);
     }
 
-    public CompletableFuture<Void> scanRecursive(Path dir, String mask) {
+    private CompletableFuture<Void> scanRecursive(Path dir, String mask, List<FileEntity> currentScanResults) {
         return CompletableFuture.runAsync(() -> {
             ArrayList<CompletableFuture<Void>> subTasks = new ArrayList<>();
 
             try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
                 for (Path entry : stream) {
                     if (Files.isDirectory(entry)) {
-                        subTasks.add(scanRecursive(entry, mask));
+                        subTasks.add(scanRecursive(entry, mask, currentScanResults));
                     } else {
                         if (matchesMask(String.valueOf(entry.getFileName()), mask)) {
                             FileEntity fileEntity = new FileEntity();
                             fileEntity.setFileName(entry.getFileName().toString());
                             fileEntity.setPath(entry.toString());
-                            fileRepository.save(fileEntity);
+
+                            fileEntity = fileRepository.save(fileEntity);
+                            currentScanResults.add(fileEntity);
                         }
                     }
                 }
@@ -54,7 +66,7 @@ public class FileService {
         }, executor);
     }
 
-    public boolean matchesMask(String fileName, String mask) {
+    private boolean matchesMask(String fileName, String mask) {
         String regex = mask.replace(".", "\\.").replace("*", ".*");
         return fileName.matches(regex);
     }
