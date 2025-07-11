ae93ade90db4741dbf778da08512ad0f7ce6fceb
AliakseiRom <aliaksei.ramanouskij@gmail.com>
2025-07-11 12:31:55 +0300

    refactor and add spring boot actuator

--- src/main/java/com/main/multithreaded_scanner_file/dto/FileResponseDTO.java
+++ src/main/java/com/main/multithreaded_scanner_file/dto/FileResponseDTO.java
@@ -2,12 +2,13 @@ package com.main.multithreaded_scanner_file.dto;
 
 import com.main.multithreaded_scanner_file.model.FileEntity;
 import lombok.Builder;
+import lombok.Data;
 
 import java.util.List;
 
-@Builder(setterPrefix = "set", toBuilder = true)
-public record FileResponseDTO(
-        String path,
-        List<FileEntity> files
-) {
+@Data
+@Builder
+public class FileResponseDTO {
+    private String path;
+    private List<FileEntity> files;
 }
